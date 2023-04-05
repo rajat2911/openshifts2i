cc5d3401229df44f6ca1f69da0042abfe86fbaae
Rajat Verma <46910206+rajat2911@users.noreply.github.com>
2023-04-05 08:14:06 +0530

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_version = "~> 1.1.0"
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.0.0"
-
-    }
-
-  }
-  backend "local" {
-    path = "terraform-sandy/terraform.tfstate"
-
-  }
-}
-
 # Terraform Block
 terraform {
   required_version = "~> 0.15.4"
