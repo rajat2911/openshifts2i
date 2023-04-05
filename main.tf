fd06de7b7873e98d5193a639460f8e5d503df4f3
Rajat Verma <46910206+rajat2911@users.noreply.github.com>
2023-04-05 07:49:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 resource "aws_instance" "terraformdemo" {
   ami               = var.aws_ami //"ami-04ad2567c9e3d7893"
   availability_zone = var.az      //"us-east-1a"
-  subnet_id         = var.sub-net //"subnet-008b0a0d1c302b578"
-  key_name          = "terraform-key"
-  instance_type     = var.IN // "t2.small"
-  count             = 1
 }
 
